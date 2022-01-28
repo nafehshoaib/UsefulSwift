a19d803ffa00789f94d3c4bf742c9134ebe84b62
nafehshoaib <nafehshoaib14@gmail.com>
2022-01-28 12:46:07 -0500

    Added If let language feature to view extension and added preview for all new functions

--- Sources/UsefulSwift/View+If.swift
+++ Sources/UsefulSwift/View+If.swift
@@ -25,4 +25,65 @@ extension View {
             self
         }
     }
+    
+    @ViewBuilder
+    public func `if`<Variable, IfContent>(`let` variable: Variable?, _ ifTransform: (Variable, Self) -> IfContent) -> some View where IfContent: View {
+        if let variable = variable {
+            ifTransform(variable, self)
+        } else {
+            self
+        }
+    }
+    
+    @ViewBuilder
+    public func `if`<Variable, IfContent, ElseContent>(`let` variable: Variable?, _ ifTransform: (Variable, Self) -> IfContent, else elseTransform: (Self) -> ElseContent) -> some View where IfContent: View, ElseContent: View {
+        if let variable = variable {
+            ifTransform(variable, self)
+        } else {
+            elseTransform(self)
+        }
+    }
+}
+
+struct ViewIf_Previews: PreviewProvider {
+    struct PreviewView: View {
+        @State var toggle = false
+        @State var variable: Color?
+        
+        var body: some View {
+            List {
+                Text("If")
+                    .if(toggle) {
+                        $0.bold()
+                }
+                Text("If + Else")
+                    .if(toggle) {
+                        $0.foregroundColor(.blue)
+                    } else: {
+                        $0
+                            .foregroundColor(.red)
+                            .bold()
+                    }
+                Text("If let ")
+                    .if(let: variable) { variable, text in
+                        text
+                            .foregroundColor(variable)
+                    }
+                Text("If let Else")
+                    .if(let: variable) { variable, text in
+                        text
+                            .foregroundColor(variable)
+                    } else: { text in
+                        text
+                            .bold()
+                    }
+
+
+            }
+        }
+    }
+    
+    static var previews: some View {
+        PreviewView()
+    }
 }
