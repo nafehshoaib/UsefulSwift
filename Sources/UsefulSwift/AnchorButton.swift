285d52ab81bb26f585b34ba7885349e4ae1d5c31
nafehshoaib <nafehshoaib14@gmail.com>
2022-04-04 16:49:01 -0400

    Added ability to disable anchored button

--- Sources/UsefulSwift/AnchorButton.swift
+++ Sources/UsefulSwift/AnchorButton.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 public struct AnchorButton<Label: View>: ViewModifier {
     
+    @Binding var isEnabled: Bool
+    
     var button: Button<Label>
     
-    public init(@ViewBuilder button: () -> Button<Label>) {
+    public init(isEnabled: Binding<Bool> = .constant(true), @ViewBuilder button: () -> Button<Label>) {
+        self._isEnabled = isEnabled
         self.button = button()
     }
     
@@ -21,6 +24,7 @@ public struct AnchorButton<Label: View>: ViewModifier {
                 .frame(maxWidth: .infinity)
             button
                 .buttonStyle(.fullWidthProminent)
+                .disabled(!isEnabled)
                 .padding(20)
                 .background(Material.bar)
         }
