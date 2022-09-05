bca035736420b6d3fe5264585b214429611f88f5
Nafeh Shoaib <nafehshoaib14@gmail.com>
2022-09-05 16:01:24 -0400

    Removed HTTPMethod -> moved to SwiftCloud Added AnyEncodable

---
+++ Sources/UsefulSwift/AnyEncodable.swift
@@ -0,0 +1,23 @@
+//
+//  AnyEncodable.swift
+//  
+//
+//  Created by Nafeh Shoaib on 9/5/22.
+//
+
+import Foundation
+
+/// Type-erased Encodable struct
+/// Swift Forums source: https://forums.swift.org/t/how-to-encode-objects-of-unknown-type/12253
+struct AnyEncodable: Encodable {
+    
+    private let encodeAction: (Encoder) throws -> Void
+    
+    public init<T: Encodable>(_ wrapped: T) {
+        encodeAction = wrapped.encode
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        try encodeAction(encoder)
+    }
+}
