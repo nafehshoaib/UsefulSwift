92380eb891b66d7d1be201532144212c9c9f4902
Nafeh Shoaib <nafehshoaib14@gmail.com>
2023-11-22 12:07:13 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "UsefulSwift",
     platforms: [
         .iOS(.v15),
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
