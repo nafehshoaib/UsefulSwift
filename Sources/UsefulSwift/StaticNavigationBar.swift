b85230a4acec6054de5134e4bd165e1c0bf55a17
Nafeh Shoaib <nafehshoaib14@gmail.com>
2024-01-08 06:37:58 -0500

    Updated status bar opacity animation on scroll to iOS 17 features

--- Sources/UsefulSwift/StaticNavigationBar.swift
+++ Sources/UsefulSwift/StaticNavigationBar.swift
@@ -26,12 +26,6 @@ public struct StaticNavigationBar<Content>: View where Content: View {
     
     public var body: some View {
         VStack {
-            GeometryReader { proxy in
-                Color.clear
-                    .preference(key: NavbarScrollPreference.self,
-                                value: -proxy.frame(in: .named("card-list")).minY)
-            }
-            .frame(height: 0)
             HStack(alignment: alignment) {
                 VStack(alignment: .leading, spacing: 5.0) {
                     if let prompt = prompt {
@@ -51,31 +45,34 @@ public struct StaticNavigationBar<Content>: View where Content: View {
     }
 }
 
-public struct NavbarScrollPreference: PreferenceKey {
-    public static var defaultValue: CGFloat = .zero
-    
-    public static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) { }
-}
 
-public struct StaticNavbarModifier: ViewModifier {
-    @State private var topOpacity: Double = 0
+public struct ScrollStatusBarModifier: ViewModifier {
+    
+    @Binding var id: Int?
+    @State var opacity: Double = 0
     
     public func body(content: Content) -> some View {
         ZStack(alignment: .top) {
-            Color.clear
-                .edgesIgnoringSafeArea([.horizontal, .top])
-                .frame(height: 1)
-                .background(.ultraThinMaterial)
-                .opacity(topOpacity)
-                .zIndex(1)
+            if let id = id {
+                Color.clear
+                    .edgesIgnoringSafeArea([.horizontal, .top])
+                    .frame(height: 1)
+                    .background(.ultraThinMaterial)
+                    .opacity(opacity)
+                    .zIndex(1)
+            }
             content
         }
-        .onPreferenceChange(NavbarScrollPreference.self) { new in
-            let newOpacity = min(Double(new / 20), 1)
+        .onChange(of: id) { new in
+            guard let new = new else {
+                return
+            }
             
-            if newOpacity != topOpacity {
-                withAnimation(.easeInOut(duration: 0.1)) {
-                    self.topOpacity = newOpacity
+            let op = min(Double(new), 1)
+            
+            if op != opacity {
+                withAnimation {
+                    opacity = op
                 }
             }
         }
@@ -84,32 +81,112 @@ public struct StaticNavbarModifier: ViewModifier {
 
 extension View {
     /// Enables navbar to move with scroll view. Needs to be used with `StaticNavigationBar`.
-    public func staticNavigationBarOnScroll() -> some View {
+    public func scrollStatusBar(id: Binding<Int?>) -> some View {
         self
             .navigationBarHidden(true)
-            .modifier(StaticNavbarModifier())
+            .modifier(ScrollStatusBarModifier(id: id))
     }
 }
 
-struct StaticNavigationBar_Previews: PreviewProvider {
-    static var previews: some View {
+@available(iOS 17.0, *)
+fileprivate struct PreviewView: View {
+    
+    @State var showSheet = false
+    @State var position: Int?
+    
+    var body: some View {
         ScrollView {
-            StaticNavigationBar("Today", prompt: "Friday, April 1") {
-                Circle()
-                    .frame(width: 50, height: 50)
-            }
-        }
-        ScrollView {
-            StaticNavigationBar("Wallet", alignment: .center) {
-                Button {
-                    
-                } label: {
-                    Image(systemName: "plus.circle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+            Group {
+                StaticNavigationBar("Today", prompt: "Friday, April 1") {
+                    Circle()
+                        .frame(width: 50, height: 50)
                 }
-                .frame(height: 30)
+                .id(0)
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.blue)
+                    .frame(height: 200)
+                    .padding(20)
+                    .id(1)
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.blue)
+                    .frame(height: 200)
+                    .padding(20)
+                    .id(2)
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.blue)
+                    .frame(height: 200)
+                    .padding(20)
+                    .id(3)
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.blue)
+                    .frame(height: 200)
+                    .padding(20)
+                    .id(4)
+                    .onTapGesture {
+                        showSheet = true
+                    }
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.blue)
+                    .frame(height: 200)
+                    .padding(20)
+                    .id(5)
+                    .onTapGesture {
+                        showSheet = true
+                    }
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.blue)
+                    .frame(height: 200)
+                    .padding(20)
+                    .id(6)
+                    .onTapGesture {
+                        showSheet = true
+                    }
             }
+            .scrollTargetLayout()
+        }
+        .scrollPosition(id: $position)
+        .scrollStatusBar(id: $position)
+        .sheet(isPresented: $showSheet, content: {
+            Text("Hello World")
+        })
+    }
+}
+
+#Preview {
+    if #available(iOS 17.0, *) {
+        PreviewView()
+    } else {
+        // Fallback on earlier versions
+        Text("Hello world")
+    }
+}
+
+#Preview {
+    ScrollView {
+        StaticNavigationBar("Wallet", alignment: .center) {
+            Button {
+                
+            } label: {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            .frame(height: 30)
+        }
+    }
+}
+
+#Preview {
+    ScrollView {
+        StaticNavigationBar("Wallet", alignment: .center) {
+            Button {
+                
+            } label: {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            .frame(height: 30)
         }
     }
 }
